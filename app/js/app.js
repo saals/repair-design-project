50d6ec92a634c83dcdd8f333f813c027ea9122de
saals <salekser@gmail.com>
2022-09-22 20:49:06 +0300

    create sect.projects; slider as swiper

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,36 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
+import Swiper, { Controller, Pagination, Scrollbar, Navigation } from "swiper";
+Swiper.use([Controller, Pagination, Scrollbar, Navigation]);
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+  const swiperImg = new Swiper(".projects__swiper-img", {
+    speed: 1200,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+  });
+  const swiperTxt = new Swiper(".projects__swiper-txt", {
+    speed: 1200,
+    loop: false,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+  });
 
+  swiperTxt.controller.control = swiperImg;
+  swiperImg.controller.control = swiperTxt;
 })
