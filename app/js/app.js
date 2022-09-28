00ca3e47a3008c51174ff46a5ab17bd82b4a541a
saals <salekser@gmail.com>
2022-09-28 15:25:40 +0300

    create sect.dreams; slider as swiper

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,7 @@ import Swiper, {Controller, Pagination, Scrollbar, Navigation} from "swiper";
 Swiper.use([Controller, Pagination, Scrollbar, Navigation]);
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  // projects
   const swiperImg = new Swiper(".projects__swiper-img", {
     speed: 1200,
     loop: false,
@@ -34,5 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
   swiperTxt.controller.control = swiperImg;
   swiperImg.controller.control = swiperTxt;
+
+  // dreams
+  const swiperDreamsGround = new Swiper(".dreams__swiper-ground", {
+    speed: 1200,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination-dreams",
+      clickable: true,
+    },
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+  });
+
+  const swiperDreamsCollage = new Swiper(".dreams__swiper-collage", {
+    speed: 1200,
+    loop: false,
+  });
+
+  swiperDreamsGround.controller.control = swiperDreamsCollage;
+  swiperDreamsCollage.controller.control = swiperDreamsGround;
 
 })
