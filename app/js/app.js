149088fc9a07949ce9845b0379535ae7a94ad1a2
saals <salekser@gmail.com>
2022-11-10 21:38:22 +0300

    реализовать слайдер секции projects на нативном JS

--- app/js/app.js
+++ app/js/app.js
@@ -1,60 +1,36 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
-
-import Swiper, {Controller, Pagination, Scrollbar, Navigation} from "swiper";
+// подключение Swiper оставлено, т.к. используется в слайдере секции dreams
+import Swiper, {Controller, Pagination, Scrollbar, Navigation} from 'swiper';
+import replaceElem from './utils/replaceElem.js';
+import getDataBase from './utils/getDataBase.js';
+import initSlider from './utils/initSlider.js';
 
 Swiper.use([Controller, Pagination, Scrollbar, Navigation]);
 
 document.addEventListener('DOMContentLoaded', () => {
-  // projects
-  const swiperImg = new Swiper(".projects__swiper-img", {
+  // projects section
+  replaceElem('.projects__navigation');
+  const someData = getDataBase();
+  initSlider(someData);
+
+  // dreams section
+  const swiperDreamsGround = new Swiper('.dreams__swiper-ground', {
     speed: 1200,
     loop: false,
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination-dreams',
       clickable: true,
     },
     navigation: {
-      prevEl: ".swiper-button-prev",
-      nextEl: ".swiper-button-next",
-    },
-  });
-  const swiperTxt = new Swiper(".projects__swiper-txt", {
-    speed: 1200,
-    loop: false,
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      draggable: true,
-    },
-    navigation: {
-      prevEl: ".swiper-button-prev",
-      nextEl: ".swiper-button-next",
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
     },
   });
 
-  swiperTxt.controller.control = swiperImg;
-  swiperImg.controller.control = swiperTxt;
-
-  // dreams
-  const swiperDreamsGround = new Swiper(".dreams__swiper-ground", {
-    speed: 1200,
-    loop: false,
-    pagination: {
-      el: ".swiper-pagination-dreams",
-      clickable: true,
-    },
-    navigation: {
-      prevEl: ".swiper-button-prev",
-      nextEl: ".swiper-button-next",
-    },
-  });
-
-  const swiperDreamsCollage = new Swiper(".dreams__swiper-collage", {
+  const swiperDreamsCollage = new Swiper('.dreams__swiper-collage', {
     speed: 1200,
     loop: false,
   });
 
   swiperDreamsGround.controller.control = swiperDreamsCollage;
   swiperDreamsCollage.controller.control = swiperDreamsGround;
-
 })
---
+++ app/js/utils/getDataBase.js
@@ -0,0 +1,28 @@
+export default function getDataBase() {
+  return [
+    {
+      id: 1,
+      url: 'images/dist/slide-img/projects-1.jpg',
+      city: 'Rostov-on-Don LCD admiral',
+      area: 81,
+      time: 3.5,
+      cost: 'Upon request'
+    },
+    {
+      id: 2,
+      url: 'images/dist/slide-img/projects-2.jpg',
+      city: 'Sochi Thieves',
+      area: 105,
+      time: 4,
+      cost: 'Upon request'
+    },
+    {
+      id: 3,
+      url: 'images/dist/slide-img/projects-3.jpg',
+      city: 'Rostov-on-Don Patriotic',
+      area: 93,
+      time: 3,
+      cost: 'Upon request'
+    },
+  ]
+}
